80795371bc401a1dfad5a19f1d6ff0f63ea37cee
code-hero-projects <code.hero.email@gmail.com>
2022-08-06 00:00:34 +0100

    refactor

--- src/keys/BaseKey.py
+++ src/keys/BaseKey.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 import cv2
@@ -11,28 +12,32 @@ from keys.NoteTypeEnum import NoteTypeEnum
 
 
 class BaseKey:
-  def __init__(self, key_bound: KeyBound, lower_bound_hsv: HSV, higher_bound_hsv: HSV, key_to_press: str) -> None:
+  def __init__(self, key_bound: KeyBound, lower_bound_hsv: HSV, higher_bound_hsv: HSV, key_to_press: str, color: str) -> None:
     self.key_bound = key_bound
     self._lower_bound_hsv = lower_bound_hsv
     self._upper_bound_hsv = higher_bound_hsv
     self._key_to_press = key_to_press
+    self._color = color
     self._key_pressed = False
-    self._previous_note = NoteTypeEnum.NONE
+    self._previous_notewrwer = NoteTypeEnum.NONE
 
-  def handle_screenshot(self, image, filename_img, filename_mask: str) -> bool:
+  def write_to_file(self, screenshot_number, image, mask):
+    filename_image = f'note-{screenshot_number}-{self._color}.png'
+    filename_mask = f'note-{screenshot_number}-{self._color}-mask.png'
+    cv2.imwrite(filename_image, image)
+    cv2.imwrite(filename_mask, mask)
+
+  def handle_screenshot(self, image, screenshot_number) -> bool:
     mask = self._mask_image(image, self._lower_bound_hsv, self._upper_bound_hsv)
     
-    current_note = self._get_current_note(mask, filename_mask)
+    current_note = self._get_current_note(mask)
 
-    # if filename_img != '':
-    #   cv2.imwrite(filename_img, image)
+    # thread = threading.Thread(target=self.write_to_file, args=(screenshot_number, image, mask), daemon=True)
+    # thread.start()
 
     if current_note == NoteTypeEnum.NONE:
       self._release()
-    elif current_note == NoteTypeEnum.SINGLE_NOTE:
-      # print(f'previous {self._previous_note}')
-      # print(f'current {current_note}')
-
+    elif current_note == NoteTypeEnum.SINGLE_NOTE:      
       if self._previous_note == NoteTypeEnum.NONE:
         self._press()
       elif self._previous_note == NoteTypeEnum.LINE:
@@ -46,13 +51,11 @@ class BaseKey:
 
   def _press(self):
     if not self._key_pressed:
-      # print ('pressed')
       keyboard.press(self._key_to_press)
       self._key_pressed = True
   
   def _release(self):
     if self._key_pressed:
-      # print ('released')
       keyboard.release(self._key_to_press)
       self._key_pressed = False
 
@@ -65,21 +68,15 @@ class BaseKey:
 
     return mask
 
-  def _get_current_note(self, mask, filename_mask):
+  def _get_current_note(self, mask):
     if np.count_nonzero(mask) == 0:
       return NoteTypeEnum.NONE
     
     note = get_image_in_range(mask, KeyBound(0, 30))
     note_count = np.count_nonzero(note)
-    # print (f'note_counts: {note_count}')
     if note_count > 0:
-      # if filename_mask != '':
-      #   cv2.imwrite(filename_mask, note)
       return NoteTypeEnum.SINGLE_NOTE
     else:
       line = get_image_in_range(mask, KeyBound(80, 90))
       note_count = np.count_nonzero(line)
-      # print (f'line_counts: {note_count}')
-      # if filename_mask != '':
-      #   cv2.imwrite(filename_mask, line)
       return NoteTypeEnum.LINE
